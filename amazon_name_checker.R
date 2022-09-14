4219966c39e68a1e69c31070c30d4a3aa1ec3e8c
Andrew <74262186+cevaboyz@users.noreply.github.com>
2022-09-14 23:10:36 +0200

    Update amazon_name_checker.R
    reformatted to get past the error

--- amazon_name_checker.R
+++ amazon_name_checker.R
@@ -1,68 +1,3 @@
-##AMAZON ASIN CHECKER 0.0
-##
-##
-##
-
-
-#Loading Libraries
-library(svDialogs)
-library(pivottabler)
-library(priceR)
-library(readxl)
-library(rio)
-library(tidyverse)
-library(tibble)
-library(openxlsx)
-library(quantmod)
-library(openxlsx)
-library(gdata)
-library(lubridate)
-library(RSelenium)
-library(rvest)
-library(binman)
-library(stringr)
-library(dplyr)
-library(lubridate)
-library(rvest)
-library(stringr)
-
-
-url <- "https://www.amazon.it/dp/B07JVLQ38M?FALSE"
-
-
-test <- read_html(url)
-
-
-name <- test %>%
-  html_nodes(xpath= "//*[@id='productTitle']/text()") %>%
-  html_text()
-
-
-name <- trimws(name)
-
-real_name <- test %>%
-  html_nodes(xpath="//*[@id='productDetails_techSpec_section_1']/tbody/tr[2]/td") %>%
-  html_text()
-
-
-table <- test %>% html_table()
-
-data <- table[[3]]
-
-asin <- unlist(data[6,2])
-
-real_name <- unlist(data[2,2])
-
-sku <- unlist(data[5,2])
-
-correct_name <- ifelse(trimws(tolower(real_name)) == trimws(tolower(name)),"GOOD", "FIX")
-
-
-final <- cbind(asin,name,real_name,correct_name,sku)
-
-asin <- unlist(ASIN)
-
-
 name_checker <- function(asin){
   
   url_compositor <- paste0("https://www.amazon.it/dp/", asin)
@@ -79,15 +14,17 @@ name_checker <- function(asin){
   
   data <- table[[3]]
   
-  asin <- asin
+  asin <- unlist(data[data$X1 == "ASIN", 2])
   
-  real_name <- unlist(data[2,2])
+  asin_2 <- asin
   
-  sku <- unlist(data[5,2])
+  #real_name <- unlist(data[2,2])
+  
+  sku <- unlist(data[data$X1 == "Numero modello articolo", 2])
   
   correct_name <- ifelse(trimws(tolower(real_name)) == trimws(tolower(name)),"GOOD", "FIX")
   
-  final <- cbind(asin,name,real_name,correct_name,sku)
+  final <- cbind(asin,asin_2,name,real_name,correct_name,sku)
   
   final_df <<- as.data.frame(final)
   
@@ -98,7 +35,11 @@ name_checker <- function(asin){
 }
 
 
+asin <- c("B08X6QMR36", "B09SDHHKJZ","B0BCPB5TYQ")
 
-name_raw <- purrr::map(asin, possibly(name_checker, NA))
+
+name_raw <- purrr::map(asin, purrr::possibly(name_checker, NA))
 
 name_df <- plyr::ldply(name_raw, data.frame)
+
+write.csv(name_df, file = "data/Amazon Name.csv")
